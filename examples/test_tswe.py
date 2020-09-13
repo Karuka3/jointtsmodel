03b8d3e67b7c2f3f5a1d51d896c6f1973231a0ed
Karuka3 <karuka.piplup.0327@gmail.com>
2020-09-13 22:41:37 +0900

    Update

--- examples/test_tswe.py
+++ examples/test_tswe.py
@@ -18,24 +18,24 @@ from jointtsmodel.utils import coherence_score_uci, Hscore
 # This produces a feature matrix of token counts, similar to what
 # CountVectorizer would produce on text.
 data, _ = fetch_20newsgroups(shuffle=True, random_state=1,
-                         remove=('headers', 'footers', 'quotes'),
-                         return_X_y=True)
+                             remove=('headers', 'footers', 'quotes'),
+                             return_X_y=True)
 data = data[:1000]
 vectorizer = CountVectorizer(max_df=0.7, min_df=10,
-                            max_features=5000,
-                            stop_words='english')
+                             max_features=5000,
+                             stop_words='english')
 X = vectorizer.fit_transform(data)
 vocabulary = vectorizer.get_feature_names()
-inv_vocabulary = dict(zip(vocabulary,np.arange(len(vocabulary))))
+inv_vocabulary = dict(zip(vocabulary, np.arange(len(vocabulary))))
 
 ### Load external sentiment lexicon file ###
-lexicon_data = pd.read_excel('lexicon/prior_sentiment.xlsx')
+lexicon_data = pd.read_csv('lexicon/prior_sentiment.csv')
 lexicon_data = lexicon_data.dropna()
-lexicon_dict = dict(zip(lexicon_data['Word'],lexicon_data['Sentiment']))
+lexicon_dict = dict(zip(lexicon_data['Word'], lexicon_data['Sentiment']))
 
 ### Load word embeddings for TSWE model ###
 embeddings_index = {}
-f = open('glove.6B.100d.txt','r',encoding='utf8')
+f = open('Data/glove.6B.100d.txt', 'r', encoding='utf8')
 
 for i, line in enumerate(f):
     values = line.split()
@@ -55,13 +55,15 @@ for i, word in enumerate(vocabulary):
         embedding_matrix[i] = np.zeros(100)
 
 ### TSWE model ###
-model = TSWE(embedding_dim=100,n_topic_components=5,n_sentiment_components=5,random_state=123,evaluate_every=2)
+model = TSWE(embedding_dim=100, n_topic_components=5,
+             n_sentiment_components=5, random_state=123, evaluate_every=2)
 model.fit(X.toarray(), lexicon_dict, embedding_matrix)
 
-#model.transform()[:2]
+# model.transform()[:2]
 
 ### Evaluation ###
 top_words = list(model.getTopKWords(vocabulary).values())
-print ("Coherence {}".format(coherence_score_uci(X.toarray(),inv_vocabulary,top_words)))
-print ("Hscore {}".format(Hscore(model.transform())))
-print ("Top words \n{}".format(top_words))
+print("Coherence {}".format(coherence_score_uci(
+    X.toarray(), inv_vocabulary, top_words)))
+print("Hscore {}".format(Hscore(model.transform())))
+print("Top words \n{}".format(top_words))
